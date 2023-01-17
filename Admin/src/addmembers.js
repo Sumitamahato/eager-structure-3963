7c9f1e1f0f56cdedde7167638aa1b4b903487b9d
Deepon Ghosh <deepanghoshslg@gmail.com>
2023-01-17 17:59:11 +0530

    Login fixing

--- Admin/src/addmembers.js
+++ Admin/src/addmembers.js
@@ -46,9 +46,15 @@ menuBtn4.addEventListener('click',()=>{
 
 // Logout
 let logout=document.getElementById('logOut');
+let flag=localStorage.getItem('loginStatus')||false;
+
 
 logout.addEventListener('click',()=>{ 
     location.replace('adminLogin.html');
     flag=false;
     localStorage.setItem('loginStatus',flag);
 })
+
+if(flag=="false"){
+    window.location.href="adminLogin.html";
+}
--- Admin/src/adminLogin.js
+++ Admin/src/adminLogin.js
@@ -3,11 +3,13 @@
 let flag=localStorage.getItem('loginStatus') || false;
 let form=document.getElementById('LoginForm');
 
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     if(e.target[0].value=='admin@mail.com' && e.target[1].value=='admin'){
         flag=true;
         localStorage.setItem('loginStatus',flag);
+        console.log(localStorage.getItem('loginStatus'));
         window.location.href='dashboard.html';
     }else{
         alert('Invalid Credentials');
