b0067c7c68531550371d91c88eca8ee42f503732
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-03-05 13:59:05 +0530

    CartPage Price bug fixed

--- scripts/cartPage.js
+++ scripts/cartPage.js
@@ -12,126 +12,80 @@ let cartbox=document.getElementById("shoppingCart");
 // localStorage.setItem("cart",JSON.stringify(a));
 
 let cart=JSON.parse(localStorage.getItem("cart"))||[];
-// let totalSum=0;
-cartlist(cart)
-
-function cartlist(arr){
 let totalSum=0;
 
-    cartbox.innerHTML=null;
-    let sum=0;
+console.log(cart)
+// cartlist(cart);
+
+let CartPrice=[];
+
+const handleChange =(e, price, index)=> {
     
+    let filtered = cart.filter((el, ind)=>{
+        if(index == ind){
+            console.log(el);
+        }
+    })
+    console.log("e:",e, "price:", price)
+    CartPrice.push(e*price);
+    CartFunction(cart)
+}
+
+function CartFunction(arr){
+    cartbox.innerHTML=null;
+
     if(arr.length==0){
         cartbox.innerHTML=`<p>Your basket is currently empty !</p>`
     }else{
-        let cartItem=[];
-        arr.forEach((item)=>{
-            sum=+item.price*item.count;
-            totalSum+=item.price*item.count;
-            // console.log(sum)
-            cartItem.push( `
-            <div class="trow">
-                <div class="item">
-                    <img src=${item.image} alt="">
+    let CartPageItem = [];
+    let selectArray = [];
+
+    arr.forEach((item, index)=>{
+        CartPrice[index] = item.price*10;
+        CartPageItem.push( `
+        <div class="trow">
+            <div class="item">
+                <img src=${item.image} alt="">
+            </div>
+            <div class="itemDesc">
+                <h4>${item.title}</h4>
+                <p>${item.description.substring(0,100)}</p>
+            </div>
+            <div class="pickOrDeliv">
+                <div>
+                    <input class="delivOpt" type="radio" name="delivOpt" value="Free" checked>
+                    <label for="delivOpt" id="freeLabel"><h4><span id="free">FREE</span> In-Store Pickup</h4></label>
                 </div>
-                <div class="itemDesc">
-                    <h4>${item.title}</h4>
-                    <p>${item.description.substring(0,100)}</p>
-                </div>
-                <div class="pickOrDeliv">
-                    <div>
-                        <input class="delivOpt" type="radio" name="delivOpt" value="Free" checked>
-                        <label for="delivOpt" id="freeLabel"><h4><span id="free">FREE</span> In-Store Pickup</h4></label>
-                    </div>
-                    <div>
-                        <input class="delivOpt" type="radio" name="delivOpt" value="parcel">
-                        <label for="delivOpt"><h4>Parcel Delivery</h4></label>
-                    </div>
-                </div>
-                <div class="quantityy">
-                    <h4>Units</h4>
-                    <select data-price=${item.price} data-id="${item.id}" name="quantity"  class="selectQuant">
-                        <option value="10">10</option>
-                        <option value="30">30</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                    </select>
-                    <button>Remove</button>
-                </div>
-                <div class="price">
-                    <p>$<span class='miniPrices' data-id=${item.id}>${sum.toFixed(2)}</span></p>
+                <div>
+                    <input class="delivOpt" type="radio" name="delivOpt" value="parcel">
+                    <label for="delivOpt"><h4>Parcel Delivery</h4></label>
                 </div>
             </div>
-            `)
-        })
-        
-        
-        localStorage.setItem("totalSum",sum);
-        cartbox.innerHTML=cartItem.join('');
-
-        selectTag=document.querySelectorAll(".selectQuant");
-        let miniPrices=document.querySelectorAll('.miniPrices');
-        // selectTag.addEventListener("click",function(){
-        //     console.log(selectTag.value)
-        // })
-   
-        selectTag.forEach(function(el){
-            el.addEventListener("change",function(e){
-                let cartId=e.target.dataset.id;
-                let val=el.value;
-                totalSum=+val*sum;
-                miniPrices.forEach((element,id)=>{
-                    if(element.dataset.id==cartId){
-                        // let a=element.innerText;
-                        element.innerText=val*e.target.dataset.price;
-                    }
-                })
-                for(let i=0;i<cart.length;i++){
-                    // console.log(cartId)
-                    if(cart[i].id==cartId){
-                        totalSum=totalSum-(cart[i].count*cart[i].price);
-                        cart[i].count=val;
-                        localStorage.setItem("cart",JSON.stringify(cart));
-                        cartlist(cart)
-                        break;
-                    }
-                }
-            })
-        })
-        document.getElementById("itemInCart").innerText=`$${totalSum.toFixed(2)}`
-        document.getElementById("total").innerText=`$${totalSum.toFixed(2)}`
-    }
-
-
-
-    let removeBtn=document.querySelectorAll(".quantityy>button");
-    removeBtn.forEach((item)=>{
-        item.addEventListener("click",function(e){
-            let id=e.target.dataset.id
-            console.log(e)
-            for(let i=0;i<cart.length;i++){
-                if(cart[i].id==id){
-                    cart.splice(i,1);
-                    cartlist(cart);
-                    localStorage.setItem("cart",JSON.stringify(cart));
-                    break;
-                }
-            }
-        })
+            <div class="quantityy">
+                <h4>Units</h4>
+                <select data-price=${item.price} data-id="${item.id}" name="quantity"  class="selectQuant" onchange="handleChange(value, ${item.price}, ${index})">
+                    <option value="10">10</option>
+                    <option value="30">30</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+                <button>Remove</button>
+            </div>
+            <div class="price">
+            <p>$<span class='miniPrices' data-id=${item.id}>${CartPrice[index]}</span></p>
+            </div>
+        </div>
+        `)
     })
+
+    let select = document.getElementsByClassName("selectQuant");
+
+    cartbox.innerHTML = CartPageItem.join("")
+    console.log(CartPageItem)
 }
-
-
-
-
-
-
-
-
-
-
-
-
+}
+{/* <p>$<span class='miniPrices' data-id=${item.id}>${sum.toFixed(2)}</span></p> */}
+CartFunction(cart)
 
 
 let continueShopping=document.querySelector(".cartHeading>a");
